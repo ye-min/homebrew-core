54d89560b718c1ffdfd3198c9d40d8c2885b6bb1
Michael Cho <michael@michaelcho.dev>
2024-11-22 22:25:15 -0500

    faust 2.75.7

--- Formula/f/faust.rb
+++ Formula/f/faust.rb
@@ -1,10 +1,9 @@
 class Faust < Formula
   desc "Functional programming language for real time signal processing"
   homepage "https://faust.grame.fr"
-  url "https://github.com/grame-cncm/faust/releases/download/2.72.14/faust-2.72.14.tar.gz"
-  sha256 "dcd5aaf263c59d34c385e65c4f4c5b85b0e9435e57cbfd79bb67a01e5780acf0"
+  url "https://github.com/grame-cncm/faust/releases/download/2.75.7/faust-2.75.7.tar.gz"
+  sha256 "89cfed24e0dabfc344fd9ecea746719cc3dd274f1a8ac283da760f59bf009c87"
   license "GPL-2.0-or-later"
-  revision 2
 
   # Upstream creates releases that use a stable tag (e.g., `v1.2.3`) but are
   # labeled as "pre-release" on GitHub before the version is released, so it's
@@ -23,42 +22,51 @@ class Faust < Formula
   end
 
   depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
+  depends_on "pkgconf" => :build
   depends_on "libmicrohttpd"
   depends_on "libsndfile"
-  depends_on "llvm@18"
+  depends_on "llvm"
+  depends_on "zstd"
 
-  fails_with gcc: "5"
+  # Backport support for LLVM 19
+  patch do
+    url "https://github.com/grame-cncm/faust/commit/5b275806093fb2e3348043de1150f01ff892d232.patch?full_index=1"
+    sha256 "f5458dae770b6b96da7ebe22e8b928be6f2e4932b5af6fe7af9fa1121edd1ab6"
+  end
 
   def install
-    system "cmake", "-S", "build", "-B", "homebrew_build",
-                    "-DC_BACKEND=COMPILER DYNAMIC",
-                    "-DCODEBOX_BACKEND=COMPILER DYNAMIC",
-                    "-DCPP_BACKEND=COMPILER DYNAMIC",
-                    "-DCMAJOR_BACKEND=COMPILER DYNAMIC",
-                    "-DCSHARP_BACKEND=COMPILER DYNAMIC",
-                    "-DDLANG_BACKEND=COMPILER DYNAMIC",
-                    "-DFIR_BACKEND=COMPILER DYNAMIC",
-                    "-DINTERP_BACKEND=COMPILER DYNAMIC",
-                    "-DJAVA_BACKEND=COMPILER DYNAMIC",
-                    "-DJAX_BACKEND=COMPILER DYNAMIC",
-                    "-DJULIA_BACKEND=COMPILER DYNAMIC",
-                    "-DJSFX_BACKEND=COMPILER DYNAMIC",
-                    "-DLLVM_BACKEND=COMPILER DYNAMIC",
-                    "-DOLDCPP_BACKEND=COMPILER DYNAMIC",
-                    "-DRUST_BACKEND=COMPILER DYNAMIC",
-                    "-DTEMPLATE_BACKEND=OFF",
-                    "-DWASM_BACKEND=COMPILER DYNAMIC WASM",
-                    "-DINCLUDE_EXECUTABLE=ON",
-                    "-DINCLUDE_STATIC=OFF",
-                    "-DINCLUDE_DYNAMIC=ON",
-                    "-DINCLUDE_OSC=OFF",
-                    "-DINCLUDE_HTTP=OFF",
-                    "-DOSCDYNAMIC=ON",
-                    "-DHTTPDYNAMIC=ON",
-                    "-DINCLUDE_ITP=OFF",
-                    "-DITPDYNAMIC=ON",
-                    *std_cmake_args
+    args = [
+      "-DC_BACKEND=COMPILER DYNAMIC",
+      "-DCODEBOX_BACKEND=COMPILER DYNAMIC",
+      "-DCPP_BACKEND=COMPILER DYNAMIC",
+      "-DCMAJOR_BACKEND=COMPILER DYNAMIC",
+      "-DCSHARP_BACKEND=COMPILER DYNAMIC",
+      "-DDLANG_BACKEND=COMPILER DYNAMIC",
+      "-DFIR_BACKEND=COMPILER DYNAMIC",
+      "-DINTERP_BACKEND=COMPILER DYNAMIC",
+      "-DJAVA_BACKEND=COMPILER DYNAMIC",
+      "-DJAX_BACKEND=COMPILER DYNAMIC",
+      "-DJULIA_BACKEND=COMPILER DYNAMIC",
+      "-DJSFX_BACKEND=COMPILER DYNAMIC",
+      "-DLLVM_BACKEND=COMPILER DYNAMIC",
+      "-DOLDCPP_BACKEND=COMPILER DYNAMIC",
+      "-DRUST_BACKEND=COMPILER DYNAMIC",
+      "-DTEMPLATE_BACKEND=OFF",
+      "-DWASM_BACKEND=COMPILER DYNAMIC WASM",
+      "-DINCLUDE_EXECUTABLE=ON",
+      "-DINCLUDE_STATIC=OFF",
+      "-DINCLUDE_DYNAMIC=ON",
+      "-DINCLUDE_OSC=OFF",
+      "-DINCLUDE_HTTP=OFF",
+      "-DOSCDYNAMIC=ON",
+      "-DHTTPDYNAMIC=ON",
+      "-DINCLUDE_ITP=OFF",
+      "-DITPDYNAMIC=ON",
+    ]
+    # Avoid linkage to `z3` when using latest `llvm`
+    args += %w[EXE SHARED].map { |type| "-DCMAKE_#{type}_LINKER_FLAGS=-Wl,-dead_strip_dylibs" } if OS.mac?
+
+    system "cmake", "-S", "build", "-B", "homebrew_build", *args, *std_cmake_args
     system "cmake", "--build", "homebrew_build"
     system "cmake", "--install", "homebrew_build"
 

e9d9227b8f53ac9a118c609940e0afd8ee857f5e
Michael Cho <michael@michaelcho.dev>
2024-11-29 21:21:37 -0500

    cortexso 1.0.3

--- Formula/c/cortexso.rb
+++ Formula/c/cortexso.rb
@@ -1,10 +1,15 @@
 class Cortexso < Formula
   desc "Drop-in, local AI alternative to the OpenAI stack"
-  homepage "https://jan.ai/cortex"
-  url "https://registry.npmjs.org/cortexso/-/cortexso-0.1.1.tgz"
-  sha256 "48efc16761eebfdd60e50211049554e7b781b30e56461042c6bf100e84d8d244"
+  homepage "https://cortex.so/"
+  url "https://github.com/janhq/cortex.cpp/archive/refs/tags/v1.0.3.tar.gz"
+  sha256 "dcf02f662ffc11a4925826db99d408d44f01db64bec3c888a9b9e05f3e747654"
   license "Apache-2.0"
-  head "https://github.com/janhq/cortex.git", branch: "dev"
+  head "https://github.com/janhq/cortex.cpp.git", branch: "dev"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+  end
 
   bottle do
     rebuild 1
@@ -18,37 +23,76 @@ class Cortexso < Formula
     sha256 cellar: :any_skip_relocation, x86_64_linux:   "f02d924016dcab7dce2faecaac5a77ba04ad4ba20dcac3e0fcc753d248364264"
   end
 
-  depends_on "node"
+  depends_on "cmake" => :build
+  depends_on "cli11" => :build
+  depends_on "cpp-httplib" => :build
+  depends_on "eventpp" => :build
+  depends_on "indicators" => :build
+  depends_on "tabulate" => :build
 
-  on_linux do
-    # Workaround for old `node-gyp` that needs distutils.
-    # TODO: Remove when `node-gyp` is v10+
-    depends_on "python-setuptools" => :build
-  end
+  depends_on "drogon"
+  depends_on "jsoncpp"
+  depends_on "libarchive"
+  depends_on "minizip"
+  depends_on "openssl@3"
+  depends_on "sqlite"
+  depends_on "sqlitecpp"
+  depends_on "yaml-cpp"
+
+  uses_from_macos "curl"
 
   conflicts_with "cortex", because: "both install `cortex` binaries"
 
-  def install
-    system "npm", "install", *std_npm_args
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+  # No tags. For now, using same provided by vcpkg submodule
+  resource "hwinfo" do
+    url "https://github.com/lfreist/hwinfo/archive/46690dd36727b868c5bb7a7316bb2ee52a898349.tar.gz"
+    sha256 "3fbee876de242f21b1f9db1949907d4a92b7952f903efaf45d9df500bd9c7a48"
+  end
 
-    # Remove incompatible pre-built binaries
-    os = OS.kernel_name.downcase
-    arch = Hardware::CPU.intel? ? "x64" : Hardware::CPU.arch.to_s
-    node_modules = libexec/"lib/node_modules/cortexso/node_modules/cpu-instructions/prebuilds"
-    node_modules.each_child do |dir|
-      rm_r(dir) if dir.basename.to_s != "#{os}-#{arch}"
+  def install
+    resource("hwinfo").stage do
+      system "cmake", "-S", ".", "-B", "build",
+                      "-DBUILD_EXAMPLES=OFF",
+                      "-DBUILD_TESTING=OFF",
+                      "-DHWINFO_SHARED=OFF",
+                      "-DHWINFO_STATIC=ON",
+                      *std_cmake_args(install_prefix: buildpath/"hwinfo")
+      system "cmake", "--build", "build"
+      system "cmake", "--install", "build"
     end
+
+    inreplace ["engine/CMakeLists.txt", "engine/cli/CMakeLists.txt"] do |s|
+      # Do not statically link to OpenSSL library
+      s.gsub! "set(OPENSSL_USE_STATIC_LIBS TRUE)", ""
+
+      # Work around the usage of a vcpkg-specific CMake file
+      s.gsub! "find_package(unofficial-minizip CONFIG REQUIRED)", <<~CMAKE
+        find_library(MINIZIP_LIBRARY NAMES minizip REQUIRED)
+        add_library(unofficial::minizip::minizip UNKNOWN IMPORTED)
+        set_target_properties(unofficial::minizip::minizip PROPERTIES IMPORTED_LOCATION "${MINIZIP_LIBRARY}")
+      CMAKE
+    end
+
+    # Set CMAKE_MODULE_PATH to avoid copy of FindSqlite3 in `drogon`
+    args = %W[
+      -DCMAKE_MODULE_PATH=#{Formula["cmake"].opt_pkgshare}/Modules
+      -Dlfreist-hwinfo_DIR=#{buildpath}/hwinfo/lib/cmake/hwinfo
+    ]
+
+    system "cmake", "-S", "engine", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    # FIXME: There are no CMake installation rules
+    bin.install "build/cortex", "build/cortex-server"
   end
 
   test do
     port = free_port
-    pid = fork { exec bin/"cortex", "serve", "--port", port.to_s }
-    sleep 10
+    system bin/"cortex", "start", "--port", port.to_s
     begin
-      assert_match "OK", shell_output("curl -s localhost:#{port}/v1/health")
+      sleep 10
+      assert_match "cortex-cpp is alive", shell_output("curl http://127.0.0.1:#{port}/healthz")
     ensure
-      Process.kill "SIGTERM", pid
+      system bin/"cortex", "stop"
     end
   end
 end

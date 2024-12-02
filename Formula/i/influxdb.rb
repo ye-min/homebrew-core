844426b1f688f3475dae26265a1a99539e1211a9
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2024-12-02 23:02:31 +0000

    influxdb 2.7.11

--- Formula/i/influxdb.rb
+++ Formula/i/influxdb.rb
@@ -4,8 +4,8 @@ class Influxdb < Formula
   # When bumping to 3.x, update license stanza to `license any_of: ["Apache-2.0", "MIT"]`
   # Ref: https://github.com/influxdata/influxdb/blob/main/Cargo.toml#L124
   url "https://github.com/influxdata/influxdb.git",
-      tag:      "v2.7.10",
-      revision: "f302d9730c3c66577bea7bc7199cfae773bf308e"
+      tag:      "v2.7.11",
+      revision: "fbf5d4ab5e65d3a3661aa52e1d05259d19a6a81b"
   license "MIT"
   head "https://github.com/influxdata/influxdb.git", branch: "main-2.x"
 
